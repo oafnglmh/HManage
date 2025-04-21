cde1cfd2009b2ee4f65d55c3346348fb81bc9480
oafnghh <minhhoanglee11@gmail.com>
2025-04-21 23:01:24 +0700

    init prj

---
+++ hmanage_be/src/test/java/com/hmanage/hmanage_be/HmanageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmanage.hmanage_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmanageBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
